5badee320d490b52baf24cab6ea7879021a0d632
kyweez <auboudier.pro@gmail.com>
2020-01-15 09:04:23 +0100

    Initial commit of the Dec_To_Bin project

---
+++ Dec_To_Bin/src/DecToBinProgram.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Kyweez
+ *
+ */
+public class DecToBinProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		Scanner sc;
+		
+		sc = new Scanner(System.in);
+		
+		
+		sc.close();
+	}
+
+}
---
+++ Dec_To_Bin/src/names/Cst.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package names;
+
+/**
+ * @author Kyweez
+ *
+ */
+public class Cst
+{
+	public static final String PROGRAM_NAME = "DECIMAL TO BINARY CONVERTER";
+	public static final String PROGRAM = "PROGRAM";
+	public static final String NUMBER = "decimal number";
+}
