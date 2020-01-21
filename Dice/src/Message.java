3d6438d20d146c7eb31886c5088bfa6d103a5a09
kyweez <auboudier.pro@gmail.com>
2020-01-21 14:21:01 +0100

    Utilisation du next au lieu du nextline pour le scanner. Le next line enregistre la ligne tapee et reinitialise le buffer.

--- Dice/src/Message.java
+++ Dice/src/Message.java
@@ -18,4 +18,12 @@ public class Message
 	public void	nameOfPlayer(int i) {
 		System.out.println("Enter the player " + (i+1) + " name :");
 	}
+	
+	public void	askRollDice() {
+		System.out.println("Please press \'Enter\' to roll the dice");
+	}
+	
+	public void	askPlayer(String playerName) {
+		System.out.println(playerName + ", it's your turn to play");
+	}
 }
--- Dice/src/Scanning.java
+++ Dice/src/Scanning.java
@@ -6,7 +6,7 @@ public class Scanning {
 
 		for (int i = 0; i < nb; i++) {
 			output.nameOfPlayer(i);
-			tab[i] = new Player(sc.nextLine(), i, 3, 0);
+			tab[i] = new Player(sc.next(), i, 3, 0);
 		}
 	}
 
@@ -17,14 +17,14 @@ public class Scanning {
 		while (!validNumber) {
 			output.numberOfPlayers();
 			if (sc.hasNextInt()) {
-				numberOfPlayers = Integer.parseInt(sc.nextLine());
+				numberOfPlayers = Integer.parseInt(sc.next());
 				if (numberOfPlayers >= 1 && numberOfPlayers <= 6)
 					validNumber = true;
 				else
 					output.errorPlayerNumber();
 			} else {
 				output.errorPlayerNumber();
-				sc.nextLine();
+				sc.next();
 			}
 		}
 		return (numberOfPlayers);
